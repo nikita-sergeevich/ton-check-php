c50f622230005a9369ef6ad8aae7908cd03bc2b6
nikita-sergeevich <developer.nikita.sergeevich@gmail.com>
2024-05-20 04:10:15 +0300

    new file:   back.php 	new file:   index.html 	new file:   tonconnect-manifest.json

---
+++ back.php
@@ -0,0 +1,49 @@
+<?php
+//	$link = mysqli_connect("localhost", "user", "password", "database"); // db connect
+
+	function check_proof($ton_proof, $account) {
+		
+		//data pre-processing
+		$account["address"] = str_replace("0:", "", $account["address"]); //remove extra characters
+		$workchain = 0; //default for workchain
+		$address = hex2bin($account["address"]);
+		$public_key = hex2bin($account["publicKey"]);
+		$timestamp = $ton_proof["timestamp"]; 
+		$domain_length = $ton_proof["domain"]["lengthBytes"];
+		$domain_value = $ton_proof["domain"]["value"];
+		$signature = base64_decode($ton_proof["signature"]);
+		$payload = $ton_proof["payload"];
+
+		//preparing a message
+		$message = "ton-proof-item-v2/";
+		$message .= pack("V", $workchain);  // 4 bytes, little-endian
+		$message .= $address;
+		$message .= pack("V", $domain_length); // 4 bytes, little-endian
+		$message .= $domain_value;
+		$message .= pack("P", $timestamp); // 8 bytes, little-endian
+		$message .= $payload;
+
+		//signature creation
+		$hashed_message = hash("sha256", $message, true);
+		$signature_message = "\xFF\xFF" . "ton-connect" . $hashed_message;
+		$hashed_signature_message = hash("sha256", $signature_message, true);
+
+		//signature verification
+		$valid = sodium_crypto_sign_verify_detached($signature, $hashed_signature_message, $public_key);
+
+		return $valid;
+	}
+
+    if (!empty($_POST["wallet"]))
+    {
+    	$isValid = check_proof($_POST["proof"], $_POST["account"]);
+
+	    if ($isValid) {
+			//do upon successful signature verification, for example:
+//			$query = mysqli_query($link, "UPDATE `table` SET `ton-wallet`='".$_POST["wallet"]."' WHERE `link`='".$_POST["proof"]["payload"]."'"); //recording a user-friendly wallet in the database
+	    } else {
+	        //do if signature verification fails
+	    }
+    }
+	
+?>
